85b3c895230c6b852382dc3fd6a5999adc72efcf
Sam Paredes <42664605+CoffeeJunkiee@users.noreply.github.com>
2020-06-21 18:14:28 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,14 @@
+# Subvenkon
+
+Subvenkon is a subdomain enumerator which gathers information from [Venkon](https://www.venkon.us/subdomain-lister/). Venkon mostly is a web interface where the user has to provide the domain and venkon will give the user the results of the subdomains available through its web interface. Subvenkon makes this easier due to its usage straight from the terminal without the need of a browser. Also, I ommited the tool banner when the tool is working so it's easier to pipe the results in different scenarios. 
+
+# Usage
+
+Subvenkon will enumerate the domain in the same way as [Venkon](https://www.venkon.us/subdomain-lister/) does its job. Once the domain is provided Subvenkon will print the available subdomains and will create a file with the name of the target so the user can pipe it, or just save the results.  
+
+For the sake of this example, I will be using a domain from a public program from Hackerone which is [AT&T](https://hackerone.com/att).
+
+```python subvenkon.py -d sky.com.mx```
+<img src="{{ site.url }}{{ site.baseurl }}example.gif" alt="oscp list">
+
+
---
+++ subvenkon.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import base64
+import os
+import sys
+import getopt
+import requests
+import re
+import subprocess
+
+target = ''
+output = ''
+
+def usage():
+    print ""
+    print "┌─┐┬ ┬┌┐ ┬  ┬┌─┐┌┐┌┬┌─┌─┐┌┐┌"
+    print "└─┐│ │├┴┐└┐┌┘├┤ │││├┴┐│ ││││ By: Sam Paredes(CoffeeJunkie)"
+    print "└─┘└─┘└─┘ └┘ └─┘┘└┘┴ ┴└─┘┘└┘ Twitter: @coffeejunkiee_"
+    print ""
+    print "Usage: subvenkon.py -d redacted.com"
+    print "-h --help                           - Help"
+    print "-d --domain                         - Domain to gather subdomains"
+    print "Example:"
+    print "subvenkon.py -d redacted.com"
+    sys.exit(0)
+
+def run_scan(scan, stderr=None):
+    return subprocess.check_output(scan, shell=True, stderr=stderr, universal_newlines=True)
+
+def main():
+    global target
+    global output
+    
+    if not len(sys.argv[1:]):
+        print usage()
+    
+    try:
+        opts, args = getopt.getopt(sys.argv[1:],"h:d:",["help","domain"])
+    except getopt.GetoptError as err:
+            print str(err)
+            usage()
+    
+    for o,a in opts:
+        if o in ("-h", "--help"):
+            usage()
+        elif o in ("-d", "--domain"):
+            target   = a   
+        else:
+            assert False, "Unhandled Option"
+main()    
+
+
+def input_work():
+
+    global target
+
+    if not len(target):
+        print ""
+        print "Something wrong has happened."
+        print "Please type 'python subvenkon.py -h' for more options."
+    elif target is not None:
+        encoder =  base64.b64encode(bytes(target))
+        url = "https://cloud.venkon.us/subdomain-lister-process/%s/dGVzdEB0ZXN0LmNvbQ=="%(encoder)
+        r = requests.get(url)
+        if "Done scan subdomains" in r.text:
+            text = r.text
+            start = text.find('loadJson("') + 10
+            end = text.find('");', start)
+            found = text[start:end]
+            url2 = "https://cloud.venkon.us/subdomain-lister-report/%s"%(found)
+            r2 = requests.get(url2)
+            text2 = r2.text
+            grep = "echo '%s' | grep '%s' | tr -d '                                    '"%(text2, target)
+            run_scan(grep)
+            grep2 = '''echo "%s"|  awk '{gsub("tr", "");print}' |  awk '{gsub("td", "");print}' |  awk '{gsub("h3", "");print}' |  awk '{gsub("<>", "");print}' |  awk '{gsub("</>", "");print}' |  awk '{gsub("<divclass=", "");print}' | awk '{gsub("TargetDomain:&nbsp;<songclass=", "");print}' |  awk '{gsub("</div>", "");print}'  | awk '{gsub("</song>", "");print}' |  awk '{gsub(">", "");print}' |  awk '{gsub("info-2-data", "");print}' |  awk '{gsub("target-host", "");print}' | tr -d '"'| awk '{gsub("&lt;BR&gt;", "\\n");print}' |  uniq  | tee -a %s-subvenkon.txt''' % (run_scan(grep),target)
+            print run_scan(grep2)
+        else:
+            print "\nDid you type the correct domain? Try again!"
+input_work()
